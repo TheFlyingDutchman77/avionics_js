361864ce881fe964cbf449559f7d85953ac8240e
TheFlyingDutchman77 <72393912+TheFlyingDutchman77@users.noreply.github.com>
2021-01-20 20:01:33 +0100

    Update index.js

--- src/avionics/index.js
+++ src/avionics/index.js
@@ -61,7 +61,7 @@ class Avionics {
     this._pitchValue = 0;
     this._airspeed = 0;
     this._altitube = 0;
-    this._heading = 360;
+    this._heading = 180;
     this._verticalSpeed = 0;
   }
 
